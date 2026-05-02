ecc044783ce407bb256faf56591f4d22a6bdfac9
E.FU <e-fu@users.noreply.github.com>
2026-05-02 15:17:06 +0800

    Add expired filter recovery test for Cartouche.Filter

--- test/filter_test.exs
+++ test/filter_test.exs
@@ -4,6 +4,34 @@ defmodule Cartouche.FilterTest do
 
   doctest Cartouche.Filter
 
+  defmodule ExpiredFilterClient do
+    def request(%Finch.Request{body: body}, _finch_name, _opts) do
+      %{"method" => method, "params" => params, "id" => id} = Jason.decode!(body)
+
+      response =
+        case {method, params} do
+          {"eth_newFilter", _} ->
+            filter_id =
+              case Process.get(:new_filter_count, 0) do
+                0 -> "0xf11735"
+                _ -> "0xf11736"
+              end
+
+            Process.put(:new_filter_count, Process.get(:new_filter_count, 0) + 1)
+            %{jsonrpc: "2.0", result: filter_id, id: id}
+
+          {"eth_getFilterChanges", ["0xf11735"]} ->
+            Process.put(:expired_seen, true)
+            %{jsonrpc: "2.0", error: %{code: -32_000, message: "filter not found"}, id: id}
+
+          {"eth_getFilterChanges", ["0xf11736"]} ->
+            %{jsonrpc: "2.0", result: Cartouche.Test.Client.eth_getFilterChanges("0xf11735"), id: id}
+        end
+
+      {:ok, %Finch.Response{status: 200, body: Jason.encode!(response)}}
+    end
+  end
+
   test "add a filter and get events" do
     extra_data = %{some_key: "some value"}
 
@@ -50,5 +78,46 @@ defmodule Cartouche.FilterTest do
     assert_received {:log, ^log}
   end
 
-  # TODO(Task 58): Test expired filter
+  test "recreates filter when ethereum node reports expired filter" do
+    Process.put(:new_filter_count, 0)
+    Process.delete(:expired_seen)
+
+    extra_data = %{some_key: "some value"}
+
+    log =
+      %{
+        "address" => "0xb5a5f22694352c15b00323844ad545abb2b11028",
+        "blockHash" => "0x99e8663c7b6d8bba3c7627a17d774238eae3e793dee30008debb2699666657de",
+        "blockNumber" => "0x5d12ab",
+        "data" => "0x00000000000000000000000000000000000000000000000000000004a817c800",
+        "logIndex" => "0x0",
+        "removed" => false,
+        "topics" => [
+          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+          "0x000000000000000000000000b2b7c1795f19fbc28fda77a95e59edbb8b3709c8",
+          "0x0000000000000000000000007795126b3ae468f44c901287de98594198ce38ea"
+        ],
+        "transactionHash" => "0xa74c2432c9cf7dbb875a385a2411fd8f13ca9ec12216864b1a1ead3c99de99cd",
+        "transactionIndex" => "0x3"
+      }
+      |> Cartouche.Filter.Log.deserialize()
+      |> Map.put(:extra_data, extra_data)
+
+    {:ok, _filter_pid} =
+      Cartouche.Filter.start_link(
+        name: ExpiredFilter,
+        address: <<1::160>>,
+        events: ["Transfer(address indexed from, address indexed to, uint amount)"],
+        check_delay: 20,
+        extra_data: extra_data,
+        rpc_opts: [client: ExpiredFilterClient]
+      )
+
+    Cartouche.Filter.listen(ExpiredFilter)
+
+    assert_received {:event, {"Transfer", _}, ^log}, 500
+    assert_received {:log, ^log}, 500
+    assert Process.get(:expired_seen)
+    assert Process.get(:new_filter_count) >= 2
+  end
 end
